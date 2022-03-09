44270521c318add612f48e6e48b8a82c996064cf
ST-Blank <thapasagar81234@gmail.com>
2022-03-09 15:05:57 +0545

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,93 +1,19 @@
 import bodyParser from 'body-parser';
 import express, { Express, Request, Response } from 'express';
-import { Items } from './Contract/items';
-import { findAll, findById, Create,Delete,Update } from './Service/items.service';
 import { students } from './students';
+import router from './Router/routes'
 const app: Express = express();
 
 const port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.get('/students', (req: Request, res: Response) => {
-  res.status(200).json(students);
-});
+app.use('/students',router );
 
 // ITEMS Endpoint
 
-// GET /items
-app.get('/items', async (req: Request, res: Response) => {
-  try {
-    const items: Array<Items> = await findAll();
-    res.status(200).json(items);
-  } catch (error) {
-    res.status(500).send('Internal Error');
-  }
-});
+app.use('/items',router)
 
-// GET items/:id
-app.get('/items/:id', async (req: Request, res: Response) => {
-  // Get the param from the path
-  const id: number = Number(req.params.id);
-  try {
-    // Fetch the item
-    const item = await findById(id);
-    if (!item) {
-      res.status(404).send('Item not found');
-    }
-    res.status(200).json(item);
-  } catch (error) {
-   
-  }
-});
-
-// POST /items
-app.post('/items', async (req: Request, res: Response) => {
-  
-  const newItem={
-    id:Math.floor(Math.random() * 1000),
-    title:req.body.title,
-    body:req.body.body
-    }
-  try {
-    const newitem=await Create(newItem)
-    res.status(200).json(newitem);
-  } catch (error) {
-     res.status(500).send('Internal Error');
-  }
-});
-
-//PUT /items/:id
-//Get the param from the path
-app.put('/items/:id', async (req: Request, res: Response) => {
-  try {
-    const id: number = Number(req.params.id);
-  const upitem=req.body
-   const updateditem=await Update(id,upitem)
-  //const array=req.body
-    //console.log(updateitem);
-  res.status(200).json(updateditem)
-  } catch (error) {
-    res.status(500).send('internal error')
-  }
-
-    
-  })
-  
-   
-    
-
-// DELETE /items
-
-app.delete('/items/:id', async (req: Request, res: Response) => {
-  const id: number = Number(req.params.id);
-  try {
-     const Item=await Delete(id)
-  res.status(200).json(Item)
-  } catch (error) {
-    res.status(500).send('Internal Error');
-  } 
-});
 
 // Start server
 app.listen(port, () => {
---
+++ src/controller/controller.ts
@@ -0,0 +1,72 @@
+import express, { Express, Request, Response } from 'express';
+import { Items } from '../Contract/items';
+import { findAll, findById, Create,Delete,Update } from '../Service/items.service';
+import { students } from '../students';
+
+export const getStudents=(req: Request, res: Response) => {
+  res.status(200).json(students);
+}
+
+export const getItem=async(req: Request, res: Response) => {
+    try {
+      const items: Array<Items> = await findAll();
+      res.status(200).json(items);
+    } catch (error) {
+      res.status(500).send('Internal Error');
+    }
+  }
+
+  export const getItembyId=async (req: Request, res: Response) => {
+    // Get the param from the path
+    const id: number = Number(req.params.id);
+    try {
+      // Fetch the item
+      const item = await findById(id);
+      if (!item) {
+        res.status(404).send('Item not found');
+      }
+      res.status(200).json(item);
+    } catch (error) {
+     
+    }
+  }
+
+  export const postItem= async (req: Request, res: Response) => {
+  
+    const newItem={
+      id:Math.floor(Math.random() * 1000),
+      title:req.body.title,
+      body:req.body.body
+      }
+    try {
+      const newitem=await Create(newItem)
+      res.status(200).json(newitem);
+    } catch (error) {
+       res.status(500).send('Internal Error');
+    }
+  }
+
+  export const updateItemByid= async (req: Request, res: Response) => {
+    try {
+      const id: number = Number(req.params.id);
+    const upitem=req.body
+     const updateditem=await Update(id,upitem)
+    //const array=req.body
+      //console.log(updateitem);
+    res.status(200).json(updateditem)
+    } catch (error) {
+      res.status(500).send('internal error')
+    }
+  
+      
+    }
+
+  export const deleteItemByid=async(req: Request, res: Response) => {
+    const id: number = Number(req.params.id);
+    try {
+       const Item=await Delete(id)
+    res.status(200).json(Item)
+    } catch (error) {
+      res.status(500).send('Internal Error');
+    } 
+  }
